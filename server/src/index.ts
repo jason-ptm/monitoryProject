ab3c677314ec19ed6f54dca609acc536c0e1beb9
Jason Solarte <js.solarte29@gmail.com>
2022-12-04 15:09:10 -0500

    configure cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import classRoomRouter from './routes/classRoom.routes'
-import assistantRouter from './routes/assistant.routes'
 
 require('./services/db')
 
 const options: cors.CorsOptions = {
-    origin: ['http://localhost:4200']
+    origin: [
+        'http://localhost:4200',
+        'https://pa-monitorias.netlify.app'
+    ]
 }
 
 
@@ -17,7 +19,6 @@ app.use(express.json())
 const PORT = 3000 || process.env.PORT
 
 app.use('/apimonitorias/classroom', classRoomRouter)
-app.use('/apimonitorias/assistant', assistantRouter)
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
